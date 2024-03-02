ad47c2bf10bd216054b68378d6268453260b460f
Sai Krishna <saikrishnakande01@gmail.com>
2024-03-02 16:20:21 +0530

    initial commit

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,14 @@
+
+function ToDoItem(props) {
+    return (
+      <div
+        onClick={() => {
+          props.onChecked(props.id);
+        }}
+      >
+        <li>{props.text}</li>
+      </div>
+    );
+  }
+  
+  export default ToDoItem;
